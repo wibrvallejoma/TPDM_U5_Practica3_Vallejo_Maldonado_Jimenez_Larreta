83730a6348f6cbf4bcce43079ae04a7073ead8d3
Brian <brsalarretaor@ittepic.edu.mx>
2019-05-31 19:03:31 -0600

    Added Listener
    A todos los botones

--- TPDM_U5_Practica3_Vallejo_Maldonado_Jimenez_Larreta/app/src/main/java/com/example/tpdm_u5_practica3_vallejo_maldonado_jimenez_larreta/MainActivity.java
+++ TPDM_U5_Practica3_Vallejo_Maldonado_Jimenez_Larreta/app/src/main/java/com/example/tpdm_u5_practica3_vallejo_maldonado_jimenez_larreta/MainActivity.java
@@ -2,12 +2,211 @@ package com.example.tpdm_u5_practica3_vallejo_maldonado_jimenez_larreta;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button CB,LSC,UD,F,DOMO,UVP,LICBI,K,P,J,LIIA,CI,
+            G,G_Prima,AD,A,A_Prima,B,B_Prima,S,Q,X,N,M,
+            L,H,Q_Prima;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CB = findViewById(R.id.CB);
+        LSC = findViewById(R.id.LSC);
+        UD = findViewById(R.id.UD);
+        F = findViewById(R.id.F);
+        DOMO = findViewById(R.id.DOMO);
+        UVP = findViewById(R.id.UVP);
+        LICBI = findViewById(R.id.LICBI);
+        K = findViewById(R.id.K);
+        P = findViewById(R.id.P);
+        J = findViewById(R.id.J);
+        LIIA = findViewById(R.id.LIIA);
+        CI = findViewById(R.id.CI);
+        G = findViewById(R.id.G);
+        G_Prima = findViewById(R.id.G_Prima);
+        AD = findViewById(R.id.AD);
+        A = findViewById(R.id.A);
+        A_Prima = findViewById(R.id.A_Prima);
+        B = findViewById(R.id.B);
+        B_Prima = findViewById(R.id.B_Prima);
+        S = findViewById(R.id.S);
+        Q = findViewById(R.id.Q);
+        X = findViewById(R.id.X);
+        N = findViewById(R.id.N);
+        M = findViewById(R.id.M);
+        L = findViewById(R.id.L);
+        H = findViewById(R.id.H);
+        Q_Prima = findViewById(R.id.Q_Prima);
+
+
+        CB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        LSC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        UD.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        F.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        DOMO.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        UVP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        LICBI.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        K.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        P.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        J.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        LIIA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        CI.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        G.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        G_Prima.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        AD.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        A.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        A_Prima.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        B.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        B_Prima.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        S.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        Q.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        X.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        N.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        M.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        L.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        H.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        Q_Prima.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
